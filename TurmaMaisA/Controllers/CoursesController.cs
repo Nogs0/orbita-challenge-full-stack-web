a040a6da4413ce9b8edc237d2e321446a7baa306
João Nogueira <joaoguinogueira04@gmail.com>
2025-10-14 00:27:16 -0300

    feat: CoursesController and StudentController added

---
+++ TurmaMaisA/Controllers/CoursesController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TurmaMaisA.Services.Courses;
+using TurmaMaisA.Services.Courses.Dtos;
+
+namespace TurmaMaisA.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseService _service;
+        public CoursesController(ICourseService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CourseDto dto)
+        {
+            return Created();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] CourseDto dto)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ TurmaMaisA/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TurmaMaisA.Services.Students;
+using TurmaMaisA.Services.Students.Dtos;
+
+namespace TurmaMaisA.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentService _service;
+        public StudentController(IStudentService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] StudentDto dto)
+        {
+            return Created();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] StudentDto dto)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
