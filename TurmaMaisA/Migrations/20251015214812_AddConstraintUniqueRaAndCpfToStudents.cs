cc933b365ac7e98c7168c1f91e17f4106f05c65a
João Nogueira <joaoguinogueira04@gmail.com>
2025-10-15 18:50:12 -0300

    feat(migrations): add migrations to set unique constraints

---
+++ TurmaMaisA/Migrations/20251015214812_AddConstraintUniqueRaAndCpfToStudents.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TurmaMaisA.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConstraintUniqueRaAndCpfToStudents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_Cpf",
+                table: "Students",
+                column: "Cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_RA",
+                table: "Students",
+                column: "RA",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Students_Cpf",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_RA",
+                table: "Students");
+        }
+    }
+}
